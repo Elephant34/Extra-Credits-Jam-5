89add45fc27d9666c3c2fc2ccb13ff403cf41e63
Ambrose Hlustik-Smith <a.hlustiksmith@gmail.com>
2019-08-24 19:37:13 +0100

    Added player animation when walking

--- scenes/player/Player.gd
+++ scenes/player/Player.gd
@@ -4,17 +4,30 @@ export (int) var speed = 200
 
 var velocity = Vector2()
 
+var movement = ""
+
 func get_input():
+	movement = ""
 	velocity = Vector2()
 	if Input.is_action_pressed('right'):
+		movement += "Right"
 		velocity.x += 1
 	if Input.is_action_pressed('left'):
+		movement += "Left"
 		velocity.x -= 1
 	if Input.is_action_pressed('down'):
+		movement += "Down"
 		velocity.y += 1
 	if Input.is_action_pressed('up'):
+		movement += "Up"
 		velocity.y -= 1
+
 	velocity = velocity.normalized() * speed
+	
+	if not movement:
+		$AnimatedSprite.stop()
+	else:
+		$AnimatedSprite.play(movement)
 
 func _physics_process(delta):
 	get_input()

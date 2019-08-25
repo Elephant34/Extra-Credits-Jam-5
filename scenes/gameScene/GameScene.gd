ea819770d6a0d7531e768b27899fb52670884b21
Ambrose Hlustik-Smith <a.hlustiksmith@gmail.com>
2019-08-25 08:58:51 +0100

    Added placeholder assets for pause menu

--- scenes/gameScene/GameScene.gd
+++ scenes/gameScene/GameScene.gd
@@ -4,5 +4,5 @@ func _ready():
 	$Player.position = globals.player_pos
 
 func _process(delta):
-	if Input.is_action_pressed("pause"):
+	if Input.is_action_just_pressed("pause"):
 		get_tree().change_scene("res://scenes/pause/PauseGUI.tscn")

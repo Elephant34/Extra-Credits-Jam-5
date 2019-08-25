6931ac4e3d84dbcabdc7387199b30739c3412f5b
Ambrose Hlustik-Smith <a.hlustiksmith@gmail.com>
2019-08-25 18:57:24 +0100

    Added condition for ghost to be stationary

--- scenes/ghost/Ghost.gd
+++ scenes/ghost/Ghost.gd
@@ -5,6 +5,6 @@ var following_player = true
 export var speed = 195
 
 func _process(delta):
-	if globals.ghost_moving:
+	if globals.ghost_moving and following_player:
 		var direction = (globals.player_pos - self.position).normalized()
 		var collision = move_and_slide(direction*speed)

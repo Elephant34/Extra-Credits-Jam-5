74e4a4aec0d4a6398c0049ccfc199e3afdb88823
Ambrose Hlustik-Smith <a.hlustiksmith@gmail.com>
2019-08-25 09:39:50 +0100

    Improved pause page

--- scenes/credits/Credits.gd
+++ scenes/credits/Credits.gd
@@ -1,8 +1,9 @@
 extends MarginContainer
 
+var game_scene = preload("res://scenes/gameScene/GameScene.tscn")
 
 func _on_BackButton_pressed():
 	if globals.credits_location == "MainMenu":
 		get_tree().change_scene("res://scenes/mainMenu/MainMenu.tscn")
 	else:
-		get_tree().change_scene("res://scenes/gameScene/gameScene.tscn")
+		get_tree().change_scene_to(game_scene)
